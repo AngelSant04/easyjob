a2053b5860bb950f0efb727fe7c935f6353d89c5
Piero Salazar <pieromental@gmail.com>
2023-01-30 11:49:09 -0500

    ACTUALIZACION

--- src/environments/globales.ts
+++
@@ -1,6 +0,0 @@
-import { Usuario } from '../app/interfaces/Usuario';
-export const usuarios:Usuario[]=[
-    { nombres: 'Angel', apellidos: 'Santamaria Herrera', celular: '97045532', fechaNacimiento: '2000-12-04', usuario: 'angel', correo: 'angelsantamaria0412@gmail.com', clave:'123'},
-    { nombres: 'Piero', apellidos: 'Salazar Calle', celular: '923937155', fechaNacimiento: '2000-12-04', usuario: 'piero', correo: 'pieromental@gmail.com', clave:'123'},
-  ]
-  
