c55b3f6153660a176f46c7e2c1d68289b1b2e31b
Piero Salazar <pieromental@gmail.com>
2023-02-05 02:29:30 -0500

    CAMBIOS

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,19 +1,13 @@
-import { Component,ViewEncapsulation } from '@angular/core';
-import SwiperCore,{ Pagination, SwiperOptions,EffectCube } from 'swiper';
-SwiperCore.use([Pagination,EffectCube])
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  encapsulation:ViewEncapsulation.None
 })
 export class Tab1Page {
 
-  configSwipe:SwiperOptions={
-   slidesPerView:1,
-   pagination:true,
-   effect:'cube'
-  }
+
   constructor() {}
 
 }
